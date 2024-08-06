60634a810a622a54ae607e53ba3098e2f452e696
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-08-06 13:44:35 -0400

    adding two sliders

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-$(function(){
+$(function () {
+  $(".slider").slick({
+    asNavFor: ".slider__text",
+    slidesToShow: 1,
+    prevArrow:
+      '<button type="button" class="slick-prev slick-arrow"><img src="/images/slider-left-btn.png" /></button>',
+    nextArrow:
+      '<button type="button" class="slick-next slick-arrow"><img src="/images/slider-right-btn.png" /></button>',
+  });
 
+  $(".slider__text").slick({
+    slidesToScroll: 1,
+    asNavFor: ".slider",
+    slidesToShow: 1,
+    fade: true,
+  });
 });
